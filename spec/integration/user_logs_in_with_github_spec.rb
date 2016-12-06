a72e659d573cbba333a81d8f65681ca38d213913
Lee Saville <l.w.saville@gmail.com>
2016-12-06 07:54:50 -0700

    Stub out login

--- spec/integration/user_logs_in_with_github_spec.rb
+++ spec/integration/user_logs_in_with_github_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 describe 'oauth login' do
   context 'user with github credentials' do
     it 'logs you in when clicking login link and redirects to dashboard' do
-      visit root_path
-      expect(response).to be_success
-      click_on "Login with Github"
+      login_with_omniauth
 
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content("Welcome to partytown")

d8de1a9c1680c2335a3cf2a9d14da5ab16ff8acc
Lee Saville <l.w.saville@gmail.com>
2016-12-05 21:14:12 -0700

    Add assignment of image to user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(uid: auth['uid']).first_or_create do |user|
       user.uid      = auth['uid']
-      user.username = auth['info']['nickname']
       user.name     = auth['info']['name']
+      user.image    = auth['info']['image']
+      user.username = auth['info']['nickname']
       user.token    = auth['credentials']['token']
       user.location = auth['extra']['raw_info']['location']
       user.joined   = auth['extra']['raw_info']['created_at']

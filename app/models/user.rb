ad5615581feae0cd2506013d9ce141c4ec8d9bd3
Lee Saville <l.w.saville@gmail.com>
2016-12-06 15:03:48 -0700

    Change method to update a user every time they log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-    where(uid: auth['uid']).first_or_create do |user|
-      user.uid      = auth['uid']
-      user.name     = auth['info']['name']
-      user.image    = auth['info']['image']
-      user.username = auth['info']['nickname']
-      user.token    = auth['credentials']['token']
-      user.location = auth['extra']['raw_info']['location']
-      user.joined   = auth['extra']['raw_info']['created_at']
-      user.save
-    end
+    user = User.find_or_create_by(uid: auth['uid'])
+    user.uid      = auth['uid']
+    user.name     = auth['info']['name']
+    user.image    = auth['info']['image']
+    user.username = auth['info']['nickname']
+    user.token    = auth['credentials']['token']
+    user.location = auth['extra']['raw_info']['location']
+    user.joined   = auth['extra']['raw_info']['created_at']
+    user.save
+    user
   end
 end

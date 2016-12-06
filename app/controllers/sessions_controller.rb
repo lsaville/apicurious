0f90b81559b6ddd8a2b4ce9c226a7798a87dfff4
Lee Saville <l.w.saville@gmail.com>
2016-12-05 21:33:08 -0700

    Add destroy action to delete session[:user_id]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    binding.pry
     if user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
     end
     redirect_to dashboard_path
+  end
+
+  def destroy
+    session[:user_id].delete
+    redirect_to root_path
   end
 end

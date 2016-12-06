b9eb1c30d9a1a4adf7d60085dbd8ea7802c9545c
Lee Saville <l.w.saville@gmail.com>
2016-12-05 20:29:58 -0700

    Add logic to create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def create
     binding.pry
+    if user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+    end
+    redirect_to dashboard_path
   end
 end

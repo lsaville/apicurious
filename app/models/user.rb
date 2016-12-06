91993f5665e2bfd70d0bf0799bd10c9f73cccf89
Lee Saville <l.w.saville@gmail.com>
2016-12-05 20:26:50 -0700

    Add from_omniauth method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.from_omniauth(auth)
+    where(uid: auth['uid']).first_or_create do |user|
+      user.uid      = auth['uid']
+      user.username = auth['info']['nickname']
+      user.name     = auth['info']['name']
+      user.token    = auth['credentials']['token']
+      user.location = auth['extra']['raw_info']['location']
+      user.joined   = auth['extra']['raw_info']['created_at']
+      user.save
+    end
+  end
 end

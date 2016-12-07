32a02ac3958a6b2ad7e4a4af633417e7b097055b
Lee Saville <l.w.saville@gmail.com>
2016-12-07 08:29:11 -0700

    Add beginning of endpoint handling for repos

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,5 +1,6 @@
 class GithubService
-  def self.repositories
-
+  def repositories
+    response = Faraday.get("https://api.github.com/user/repos?access_token=#{ENV['token']}&affiliation=owner")
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
   end
 end

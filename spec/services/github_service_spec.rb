88d400eb278212585b40ae2b459949271e24f617
Lee Saville <l.w.saville@gmail.com>
2016-12-07 16:56:15 -0700

    Change where token gets passed

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 describe 'GithubService' do
-  context '#repositories' do
-    it 'returns a collection of repositories' do
-      repos = GithubService.new.repositories
+  before(:each) do
+    @user = User.create(token: ENV['token'])
+  end
+
+  context '#repos' do
+    it 'returns a collection of repositories', :vcr do
+      repos = GithubService.new(@user.token).repos
 
       expect(repos.class).to eq(Array)
-      expect(repos.count).to eq(35)
       expect(repos.first).to have_key(:name)
-      expect(repos.first).to have_key(:id)
+      expect(repos.first).to have_key(:description)
+      expect(repos.first).to have_key(:language)
+      expect(repos.first).to have_key(:updated_at)
     end
   end
 end

fc5bfa2aa5b0d9110042489f47453240a418615f
Lee Saville <l.w.saville@gmail.com>
2016-12-07 21:34:03 -0700

    Add get_starred_repos method

--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -19,4 +19,23 @@ describe GithubRepo do
       expect(repo).to respond_to(:url)
     end
   end
+
+  context '#get_starred_repos' do
+    it 'returns an array of githubrepo objects', :vcr do
+      repos = GithubRepo.get_starred_repos(ENV['token'])
+      repo = repos.first
+
+      expect(repos).to be_an(Array)
+      expect(repo).to be_a(GithubRepo)
+      expect(repo).to respond_to(:forks_count)
+      expect(repo).to respond_to(:description)
+      expect(repo).to respond_to(:name)
+      expect(repo).to respond_to(:language)
+      expect(repo).to respond_to(:updated)
+      expect(repo).to respond_to(:forks_url)
+      expect(repo).to respond_to(:stargazers_count)
+      expect(repo).to respond_to(:stargazers_url)
+      expect(repo).to respond_to(:url)
+    end
+  end
 end

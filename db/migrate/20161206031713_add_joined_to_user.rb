589b3582edb7d62a71b0b317fc836cc5b5954cbe
Lee Saville <l.w.saville@gmail.com>
2016-12-08 13:42:16 -0700

    Remove unecessary data from user in app db

--- db/migrate/20161206031713_add_joined_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddJoinedToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :joined, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206041113) do
+ActiveRecord::Schema.define(version: 20161206031046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
-    t.string   "name"
-    t.string   "username"
     t.string   "token"
-    t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "joined"
-    t.string   "image"
   end
 
 end

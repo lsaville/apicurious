e79cf80a1a32a4691d7dc80d175d05fd4abfcd65
Lee Saville <l.w.saville@gmail.com>
2016-12-08 13:33:47 -0700

    Delete uneccessary user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     user = User.find_or_create_by(uid: auth['uid'])
     user.uid      = auth['uid']
-    user.name     = auth['info']['name']
-    user.image    = auth['info']['image']
-    user.username = auth['info']['nickname']
     user.token    = auth['credentials']['token']
-    user.location = auth['extra']['raw_info']['location']
-    user.joined   = auth['extra']['raw_info']['created_at']
     user.save
     user
   end

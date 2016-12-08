33d7730fddd1e64b09dca1dedcd8ce883de81435
Lee Saville <l.w.saville@gmail.com>
2016-12-07 21:34:42 -0700

    Add tests for followers and following methods

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,19 +1,48 @@
 require 'rails_helper'
 
 describe 'GithubService' do
-  before(:each) do
-    @user = User.create(token: ENV['token'])
-  end
-
   context '#repos' do
     it 'returns a collection of repositories', :vcr do
-      repos = GithubService.new(@user.token).repos
+      repos = GithubService.new(ENV['token']).repos
 
       expect(repos.class).to eq(Array)
       expect(repos.first).to have_key(:name)
       expect(repos.first).to have_key(:description)
       expect(repos.first).to have_key(:language)
       expect(repos.first).to have_key(:updated_at)
+    end
+  end
+
+  context '#starred' do
+    it 'returns a collection of starred repositories', :vcr do
+      repos = GithubService.new(ENV['token']).starred
+      repo = repos.first
+
+      expect(repos).to be_an(Array)
+      expect(repo).to have_key(:name)
+      expect(repo).to have_key(:url)
+      expect(repo).to have_key(:language)
+      expect(repo).to have_key(:description)
+    end
+  end
+
+  context '#followers' do
+    it 'returns a collection of followers', :vcr do
+      followers = GithubService.new(ENV['token']).followers
+      follower = followers.first
+
+      expect(followers).to be_an(Array)
+      expect(follower).to have_key(:login)
+    end
+  end
+
+  context '#following' do
+    it 'returns a collection of those the user is following', :vcr do
+      people_followed = GithubService.new(ENV['token']).following
+      one_followed = people_followed.first
+
+      expect(people_followed).to be_an(Array)
+      expect(one_followed).to have_key(:login)
     end
   end
 end

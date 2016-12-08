28174b899b2bd0b1ebcf0e3738778d17e6cd733f
Lee Saville <l.w.saville@gmail.com>
2016-12-08 13:35:16 -0700

    Create test for GithubUser

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -1,2 +1,19 @@
 require 'rails_helper'
 
+describe GithubUser do
+  context '#user_data' do
+    it 'has all the data for a user', :vcr do
+      me = GithubUser.get_user_data(ENV['token'])
+
+      expect(me).to be_a(GithubUser)
+      expect(me).to respond_to(:image)
+      expect(me).to respond_to(:name)
+      expect(me).to respond_to(:login)
+      expect(me).to respond_to(:company)
+      expect(me).to respond_to(:location)
+      expect(me).to respond_to(:email)
+      expect(me).to respond_to(:blog)
+      expect(me).to respond_to(:joined)
+    end
+  end
+end
